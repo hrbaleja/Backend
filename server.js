92ef9444237915197e6daa122760b0292a1426e0
hrbaleja <hiteshbaleja@gmail.com>
2023-10-11 12:51:01 +0530

    server

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
-// Load env variables
-// if (process.env.NODE_ENV != "production") {
-// }
-require("dotenv").config();
-
-// Import dependencies
+// Import packages
 const express = require("express");
-const cors = require("cors");
-const connectToDb = require("./config/connectToDb");
+const home = require("./routes/home");
 
-// Create an express app
+// Middlewares
 const app = express();
-
-// Configure express app
 app.use(express.json());
-app.use(cors());
-
-// Connect to database
-connectToDb();
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('hello from simple server :)')
-})
-app.use('/api/notes', require('./routes/noteRoute'));
-app.use('/api/user', require('./routes/userRoute'));
-app.use('/api/categories', require('./routes/categoryRoutes'));
+app.use("/home", home);
 
-
-// Start our server
-app.listen(process.env.PORT);
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
