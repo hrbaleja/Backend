32fa95e3b49162e33553479d2245df134c89a7c3
hrbaleja <hiteshbaleja@gmail.com>
2023-12-26 16:01:19 +0530

    New Product

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes/index.js");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // Create an express app
@@ -29,6 +30,29 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology
     console.error("Error connecting to MongoDB:", err);
     process.exit(1);
   });
+
+// Middleware for authenticating requests
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Define a protected route
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'This is a protected route', user: req.user });
+});
 
 // Define a simple root route
 app.get('/', (req, res) => {
